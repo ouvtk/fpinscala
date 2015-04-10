fed5206f1f4f521a871508b26541c45f295ad393
ouvtk <ouvtk@users.noreply.github.com>
2015-04-10 10:25:56 +0700

    Update 09.answer.scala
    When you make an exercise 8.9 there is no "MaxSize" value in Prop class. It implements later in chapter 8.3 about minimization

--- answerkey/testing/09.answer.scala
+++ answerkey/testing/09.answer.scala
@@ -1,14 +1,14 @@
 def &&(p: Prop) = Prop {
-  (max,n,rng) => run(max,n,rng) match {
-    case Passed => p.run(max, n, rng)
+  (n,rng) => run(n,rng) match {
+    case Passed => p.run(n, rng)
     case x => x
   }
 }
 
 def ||(p: Prop) = Prop {
-  (max,n,rng) => run(max,n,rng) match {
+  (n,rng) => run(n,rng) match {
     // In case of failure, run the other prop.
-    case Falsified(msg, _) => p.tag(msg).run(max,n,rng)
+    case Falsified(msg, _) => p.tag(msg).run(n,rng)
     case x => x
   }
 }
@@ -17,7 +17,7 @@ def ||(p: Prop) = Prop {
  * the given message on a newline in front of the existing message.
  */
 def tag(msg: String) = Prop {
-  (max,n,rng) => run(max,n,rng) match {
+  (n,rng) => run(n,rng) match {
     case Falsified(e, c) => Falsified(msg + "\n" + e, c)
     case x => x
   }
